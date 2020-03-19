09d570a7351c12e96e3b1d3d1aeb89dc0f1f865e
Sanket Thotange <sanket.thotange@nitorinfotech.net>
2020-03-19 16:49:31 +0530

    Data service testing

--- src/app/core/services/data.service.spec.ts
+++ src/app/core/services/data.service.spec.ts
@@ -1,11 +1,13 @@
-import { TestBed } from "@angular/core/testing";
+import { TestBed, async, inject } from "@angular/core/testing";
 import { DataService } from "./data.service";
-import { HttpClientTestingModule } from "@angular/common/http/testing";
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from "@angular/common/http/testing";
 import { defer } from "rxjs";
 
 describe("DataService", () => {
   let httpClientSpy: { get: jasmine.Spy };
-  let dataService: DataService;
   const dummyJobs: any[] = [
     {
       Id: 1,
@@ -223,7 +225,6 @@ describe("DataService", () => {
 
   beforeEach(() => {
     httpClientSpy = jasmine.createSpyObj("HttpClient", ["get"]);
-    dataService = new DataService(httpClientSpy as any);
   });
 
   it("should be created", () => {
@@ -231,35 +232,48 @@ describe("DataService", () => {
     expect(service).toBeTruthy();
   });
 
-  it("should return all job list", () => {
-    httpClientSpy.get.and.returnValue(asyncData(dummyJobs));
-    dataService.getPostedJobs(response => {
-      expect(response).toEqual(expectedDummyJobs);
-    });
-  });
+  it("should return all job list", async(
+    inject([DataService], (dataService: DataService) => {
+      httpClientSpy.get.and.returnValue(asyncData(dummyJobs));
+      dataService.getPostedJobs(response => {
+        expect(response).toEqual(expectedDummyJobs);
+      });
+    })
+  ));
 
-  it("should return expected jobs list", () => {
-    httpClientSpy.get.and.returnValue(asyncData(dummyJobs[0]));
-    dataService.getJobsById([1], response => {
-      expect(response).toEqual(expectedDummyJobs[0]);
-    });
-  });
+  it("should return expected jobs list", async(
+    inject([DataService], (dataService: DataService) => {
+      httpClientSpy.get.and.returnValue(asyncData(dummyJobs[0]));
+      dataService.getJobsById([1], response => {
+        expect(response).toEqual(expectedDummyJobs[0]);
+      });
+    })
+  ));
 
-  it("should return expected candidates list", () => {
-    httpClientSpy.get.and.returnValue(asyncData(dummyJobs[0]));
-    dataService.getShortListedCandidates(1, response => {
-      expect(response).toEqual(expectedDummyJobs[0].ShortListed);
-    });
-  });
+  it("should return expected candidates list", async(
+    inject([DataService], (dataService: DataService) => {
+      httpClientSpy.get.and.returnValue(asyncData(dummyJobs[0]));
+      dataService.getShortListedCandidates(1, response => {
+        expect(response).toEqual(expectedDummyJobs[0].ShortListed);
+      });
+    })
+  ));
 
-  it("should return expected interview details", () => {
-    httpClientSpy.get.and.returnValue(
-      asyncData(dummyJobs[0].ShortListed[0].Interview)
-    );
-    dataService.getInterviewsDetails({ CandidateId: 1, JobId: 1 }, response => {
-      expect(response).toEqual(expectedDummyJobs[0].ShortListed[0].Interview);
-    });
-  });
+  it("should return expected interview details", async(
+    inject([DataService], (dataService: DataService) => {
+      httpClientSpy.get.and.returnValue(
+        asyncData(dummyJobs[0].ShortListed[0].Interview)
+      );
+      dataService.getInterviewsDetails(
+        { CandidateId: 1, JobId: 1 },
+        response => {
+          expect(response).toEqual(
+            expectedDummyJobs[0].ShortListed[0].Interview
+          );
+        }
+      );
+    })
+  ));
 });
 function asyncData<T>(data: T) {
   return defer(() => Promise.resolve(data));
